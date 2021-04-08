eaf0cd408419cfbdabe6cf210576e684dcb99a22
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-04-08 20:44:39 +0200

    front componant

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './componants/footer/footer.component';
+import { HeaderComponent } from './componants/header/header.component';
+import { RegiserComponent } from './componants/regiser/regiser.component';
+import { StudentsComponent } from './componants/students/students.component';
+import { OptionsComponent } from './componants/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    RegiserComponent,
+    StudentsComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule
